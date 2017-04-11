84f0b9f5f8afa8f06041acf48500cb8c2098155c
Harshada Nalawade <harshada.nalawade@neosofttech.com>
2017-04-11 08:39:36 +0530

    added specific error messages

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-var app = angular.module('Books',[]);
-  app.controller('booksController',function($scope){
-    $scope.books = [
-      {title:"A Tale Of Two Cities",author:"Charles Dickens"},
-      {title:"David Copperfield",author:"Charles Dickens"},
-      {title:"Emma",author:"Jane Austen"}
-    ];
+var app = angular.module('formValidation',[]);
+  app.controller('formValidationController',function($scope){
+    
   });
